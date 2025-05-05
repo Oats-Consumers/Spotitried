99bc7df2b72f1e2b7ce5dc12a93cf1b94469ff41
imrun <turganovimrun@gmail.com>
2025-05-05 03:45:57 +0200

    work on hosting

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+env_path = Path(__file__).resolve().parent.parent / '.env'
+load_dotenv(dotenv_path=env_path)
 
-DATABASE_URL = "postgresql://oats_consumers:oats_consumed@localhost:5432/spotitried"
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+
+
+DATABASE_URL = f"postgresql://postgres.{db_user}:{db_password}@aws-0-eu-central-1.pooler.supabase.com:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
